bb1863206dc408a9f30a451b8b2e0abfe7f0b2eb
Daniel <daniel@bitbay.org>
2012-10-24 11:08:46 +0200

    webhooks route handling authentification

--- webhooks.js
+++ webhooks.js
@@ -1,4 +1,7 @@
 var pusher = require('./pusher.js'),
+	crypto = require('crypto');
+
+/*
 	PusherServer = require('pusher-server').PusherServer;
 
 var pusher_server = new PusherServer({
@@ -21,8 +24,31 @@ pusher_server.on('connect', function(){
 });
 
 pusher_server.connect();
-
+*/
 exports.hook = function(req, res){
 	console.log('webhooked');
-	res.end();
+	// environmental variable must be set
+	var app_secret = pusher.object.secret;
+
+	var app_key = req.get('X-Pusher-Key');
+	var webhook_signature = req.get('X-Pusher-Signature');
+
+	//$expected_signature = hash_hmac( 'sha256', $body, $app_secret, false );
+	var expected_signature = crypto.createHmac(crypto.SHA256, app_secret)
+		.update(req.body)
+		.digest('hex');
+		
+	if(webhook_signature == expected_signature) {
+		//$payload = json_decode($body);
+		/*
+		foreach($payload['events'] as &$event) {
+		  // do something with the event
+		}
+		*/
+		console.log(req.body);
+		res.status(200).end();
+	}
+	else {
+		res.status(401).end();
+	}
 };
