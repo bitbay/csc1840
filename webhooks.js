0abe810ef6195cc2388e31a9d59f4cf7d77a2ec3
Daniel <daniel@bitbay.org>
2012-10-24 11:51:57 +0200

    Corrected crypto,req.body is a stream

--- webhooks.js
+++ webhooks.js
@@ -2,34 +2,43 @@ var pusher = require('./pusher.js'),
 	crypto = require('crypto');
 
 exports.hook = function(req, res){
-	console.log('webhooked');
+	console.log('webhooking');
 	// environmental variable must be set
 	var app_secret = pusher.options.secret;
-
+	// key not really needed...
 	var app_key = req.get('X-Pusher-Key');
-	var webhook_signature = req.get('X-Pusher-Signature');
+	var webhook_signature = req.get('X-Pusher-Signature');	
+
+	var hmac = crypto.createHmac("sha256", app_secret);
 	
-	var payload = req.body.toString();
-	//$expected_signature = hash_hmac( 'sha256', $body, $app_secret, false );
-	var expected_signature = crypto.createHmac('sha256', app_secret)
-		.update(payload)
-		.digest('hex');
-		
-	if(webhook_signature == expected_signature) {
-		console.log(req.body);
-		for(var i=0; i< req.body.events; ++i)
-		{
-			for( key in req.body.events[i] ){
-				console.log( '-----' );
-				console.log( key );
-				console.log( req.body.events[i][key] );
-				console.log( '-----' );
+	// wait till all the data arrives...
+    req.on("data", function(data) {
+        hmac.update(data);
+    });
+
+    req.on("end", function() {
+        var crypted = hmac.digest("hex");
+
+        if(crypted === webhook_signature) {
+            // Valid request
+    		console.log(req.body);
+			for(var i=0; i< req.body.events; ++i)
+			{
+				for( key in req.body.events[i] ){
+					console.log( '-----' );
+					console.log( key );
+					console.log( req.body.events[i][key] );
+					console.log( '-----' );
+				}
 			}
-		}
-		
-		res.status(200).end();
-	}
-	else {
-		res.status(401).end();
-	}
+			res.status(200).end();
+        } else {
+            // Invalid request
+            res.status(401).end();
+        }
+    });
+
+    req.on("error", function(err) {
+        return next(err);
+    });
 };
